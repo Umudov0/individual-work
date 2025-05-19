2f3d06d9b3c5612b242b59eda65607f45f479030
umudov0 <aqif.qazax2007@gmail.com>
2025-05-19 21:43:24 +0400

    Removed unnecessary files, kept only individualwork..py

--- acm_team.py
+++
@@ -1,9 +0,0 @@
-def acm_team(t):
-    m, c = 0, 0
-    for i in range(len(t)):
-        for j in range(i+1, len(t)):
-             if s > m: m, c = s, 1
-             elif s==m: c+= 1
-    return [m, c]
-
-
